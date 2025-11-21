7af3e005c5b86a2384076c671de4d58fb06ba38e
game-radr <235256179+game-radr@users.noreply.github.com>
2025-11-21 13:34:47 +0530

    AboutUs: Update Hero

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Navbar } from "@/components";
+import Hero from "@/components/AboutUs/Hero";
 
 export const metadata: Metadata = {
   title: "About Us - Our Story",
@@ -10,14 +11,7 @@ export default function AboutUsPage() {
   return (
     <div className="min-h-screen bg-about-bg">
       <Navbar />
-      <div 
-        className="text-center"
-        style={{ paddingTop: "calc(var(--navbar-height) + 100px)" }}
-      >
-        <h1 className="font-prata font-normal text-[64px] leading-[100%] tracking-[0%] text-[#4C5637]">
-          Our Story
-        </h1>
-      </div>
+      <Hero />
     </div>
   );
 }
---
+++ components/AboutUs/Hero.tsx
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <div
+      className="text-center"
+      style={{ paddingTop: "calc(var(--navbar-height) + 100px)" }}
+    >
+      <h1 className="font-prata font-normal text-[64px] leading-[100%] tracking-[0%] text-[#4C5637]">
+        Our Story
+      </h1>
+    </div>
+  );
+}
