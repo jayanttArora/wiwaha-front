68e004aeb563c8791a969540b8680676e12229f0
game-radr <235256179+game-radr@users.noreply.github.com>
2025-11-21 12:10:11 +0530

    Update Hero

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -20,9 +20,15 @@ export default function Hero({
         className="text-center"
         style={{ paddingTop: "calc(var(--navbar-height) + 100px)" }}
       >
-        <h1 className="font-prata font-normal text-[64px] leading-[100%] tracking-[0%] text-[#FFF]">
+        <h1 className="font-prata font-normal text-[64px] leading-[75px] tracking-[0%] text-[#FFF]">
           WIWAHA BY PRAMAN
         </h1>
+        <div className="max-w-[430px] mx-auto mt-[40px] whitespace-normal">
+          <p className="font-roboto font-normal text-[20px] leading-[36px] tracking-[0%] text-[#FFFFFF] text-left">
+            Experience Balinese-themed luxury wedding venue, bespoke decor, and
+            premium tailored services for weddings & exclusive events
+          </p>
+        </div>
       </div>
     </section>
   );
