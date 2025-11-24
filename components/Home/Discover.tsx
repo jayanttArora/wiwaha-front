b1f640de1b7dee2f4093c8ed7d950e5dab2a7c62
game-radr <235256179+game-radr@users.noreply.github.com>
2025-11-24 10:43:10 +0530

    Add max-width for all sections (excluding Hero)

--- components/Home/Discover.tsx
+++ components/Home/Discover.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Discover() {
   return (
     <section className="bg-about-bg py-16">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 w-full 480:max-w-[700px] 650:max-w-[900px] 1080:max-w-[1200px] 1440:max-w-[1400px]">
         {/* Main Heading */}
         <h1 className="text-center font-prata font-normal text-[64px] leading-[100%] tracking-[0%] text-[#4C5637] mb-6">
           Discover Wiwaha
--- components/Home/WhatWeOffer.tsx
+++ components/Home/WhatWeOffer.tsx
@@ -26,7 +26,7 @@ export default function WhatWeOffer() {
 
   return (
     <section className="bg-about-bg py-16">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 w-full 480:max-w-[700px] 650:max-w-[900px] 1080:max-w-[1200px] 1440:max-w-[1400px]">
         {/* Main Heading */}
         <h1 className="text-left font-prata font-normal text-[64px] leading-[100%] tracking-[0%] text-[#4C5637] mb-6">
           What We Offer
