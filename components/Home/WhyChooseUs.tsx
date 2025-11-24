e2b85a6510b458b4cd7e95faaa42d8932ccbe03b
game-radr <235256179+game-radr@users.noreply.github.com>
2025-11-24 13:41:56 +0530

    WhyChooseUs: Update styling

--- components/Home/WhyChooseUs.tsx
+++ components/Home/WhyChooseUs.tsx
@@ -56,7 +56,7 @@ export default function WhyChooseUs() {
               className="w-[380px] h-[560px] rounded-[999px] relative -ml-[100px] -mt-[90px] z-20 p-[1px] overflow-hidden"
               style={{
                 background:
-                  "linear-gradient(167deg, rgba(210, 192, 148, 1) 0%, rgba(210, 192, 148, 1) 100%)",
+                  "linear-gradient(167deg, rgba(210, 192, 148, 1) 0%, rgba(210, 192, 148, 1) 10%, rgba(243, 238, 234, 1) 64%, rgba(243, 238, 234, 1) 100%)",
               }}
             >
               {/* Inner container with image */}
